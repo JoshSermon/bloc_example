ac42a9f701b37c08abed96a87e98cc71e48743c4
JoshSermon <JoshuaSermon1@gmail.com>
2025-12-29 23:57:19 -0600

    added more comments

--- lib/theme/theme_bloc.dart
+++ lib/theme/theme_bloc.dart
@@ -15,3 +15,4 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeMode> {
 // The `on` method is used to define the event handling logic for the `ThemeToggled` event.
 // When the `ThemeToggled` event is dispatched, the current theme state is toggled between `ThemeMode.light` and `ThemeMode.dark`.
 // The `emit` method is used to update the theme state in response to the event.
+// The initial state of the theme is set to `ThemeMode.light`.
